03b9dd5005004e08274dec631a4601315b39c43e
Matej Voboril <matej@voboril.org>
2017-07-24 23:35:50 -0500

    refactor to use ws.warframestat.us and imgur urls

--- src/embeds/VoidTraderEmbed.js
+++ src/embeds/VoidTraderEmbed.js
@@ -13,9 +13,9 @@ class VoidTraderEmbed extends BaseEmbed {
   constructor(bot, voidTrader) {
     super();
 
-    this.color = voidTrader.isActive() ? 0x0EC9FF : 0xff6961;
+    this.color = voidTrader.active ? 0x0EC9FF : 0xff6961;
 
-    if (voidTrader.isActive() || voidTrader.inventory.length > 0) {
+    if (voidTrader.active || voidTrader.inventory.length > 0) {
       this.fields = voidTrader.inventory.map(i => ({
         name: i.item,
         value: `${i.ducats} ducats + ${i.credits}cr`,
@@ -24,13 +24,13 @@ class VoidTraderEmbed extends BaseEmbed {
       this.fields = [];
     }
     this.fields.push({
-      name: `Time until ${voidTrader.isActive() ? 'departure from' : 'arrival at'} ${voidTrader.location}`,
-      value: `${voidTrader.isActive() ? voidTrader.getEndString() : voidTrader.getStartString()}` || 'Data Pending',
+      name: `Time until ${voidTrader.active ? 'departure from' : 'arrival at'} ${voidTrader.location}`,
+      value: `${voidTrader.active ? voidTrader.endString : voidTrader.startString}` || 'Data Pending',
     });
     this.title = 'Worldstate - Void Trader';
     this.description = 'Current Void Trader status';
     this.thumbnail = {
-      url: 'https://raw.githubusercontent.com/aliasfalse/genesis/master/src/resources/voidtrader.png',
+      url: 'http://i.imgur.com/z0wU29P.png',
     };
   }
 }
